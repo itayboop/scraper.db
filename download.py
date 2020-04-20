0164d1c5c1acabe52fe120334ae6a71294037522
REiNAgar <64018447+REiNAgar@users.noreply.github.com>
2020-04-20 21:16:14 +0300

    Update download.py

--- download.py
+++ download.py
@@ -2,57 +2,75 @@ import requests
 
 
 def import_table(section, start_char1, start_char2, end_char1, end_char2, path):
-    url = "https://beta.wikiversity.org/w/index.php?title=%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99%D7%9E%D7%97%D7%A9%D7%91%D7%99%D7%9D%D7%91%D7%A9%D7%99%D7%98%D7%AA%D7%91%D7%98%D7%90&action=edit&section=" + section
+	url = "https://beta.wikiversity.org/w/index.php?title=%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99_%D7%9E%D7%97%D7%A9%D7%91%D7%99%D7%9D_%D7%91%D7%A9%D7%99%D7%98%D7%AA_%D7%91%D7%98%D7%90&action=edit&section=" + section
 
-    r = requests.get(url, 'r')
+	r = requests.get(url, 'r')
 
-    page = r.content
-    page = page.decode()
+	page = r.content
+	page = page.decode()
 
-    for letter in range(len(page)):
-        if page[letter] == start_char1 and page[letter + 1] == start_char2:
-            start_of_table_index = letter
-            break
+	for letter in range(len(page)):
+		if page[letter] == start_char1 and page[letter + 1] == start_char2:
+			start_of_table_index = letter
+			break
 
-    for letter in range(len(page) - 1, 0, -1):
-        if page[letter] == end_char1 and page[letter - 1] == end_char2:
-            end_of_table_index = letter
-            break
+	for letter in range(len(page) - 1, 0, -1):
+		if page[letter] == end_char1 and page[letter - 1] == end_char2:
+			end_of_table_index = letter
+			break
 
-    page = page[start_of_table_index:end_of_table_index]
+	table = page[start_of_table_index:end_of_table_index]
 
-    with open(path, 'w') as file:
-        file.write(page)
+	with open(path, 'w') as file:
+		file.write(table)
 
 
-def username_slice(page):
-    usernames = {}
-    break_point = 0
+def username_slice(table):
+	usernames = {}
+	break_point = 0
 
-    for times in range(page.count('[[User:')):
-        f_index = page.find("[[User:", break_point) + 7
-        l_index = page.find("|", f_index)
+	for times in range(table.count('[[User:')):
+		f_index = table.find("[[User:", break_point) + 7
+		l_index = table.find("|", f_index)
 
-        break_point = l_index
+		break_point = l_index
 
-        usernames[page[f_index:l_index]] = {}
+		usernames[table[f_index:l_index]] = {}
 
-    return usernames
+	return usernames
 
 
 def read_file(path):
-    with open(path, 'r') as file:
-        table_context = file.read()
-    return table_context
+	with open(path, 'r') as file:
+		table_context = file.read()
+	return table_context
+
+
+
+def user_info(username, table_context):
+	subjects_and_points = {} 
+	table_context = table_context.split('\n')
+
+	for row in range(len(table_context)):
+		user_name_index = table_context[row].find(username)
+
+		if user_name_index != -1:
+			for subject_index in range(2, 8, 2):
+
+				subjects_and_points[table_context[row + subject_index][2:]] = table_context[row + subject_index + 1][2:]
+			return subjects_and_points
+	
 
 
 def main():
-    import_table('14', '{', '|', '}', '|', 'page_context')
-    table_context = read_file("page_context")
-    usernames = username_slice(table_context)
-
-    print(usernames.keys())
+	import_table('14', '{', '|', '}', '|', 'page_context')
+	table_context = read_file("page_context")
+	usernames = username_slice(table_context)
 
 
-if __name__ == 'main':
-    main()
+	for username in usernames.keys():
+		usernames[username] = user_info(username, table_context)
+
+
+if __name__ == '__main__':
+	main()
