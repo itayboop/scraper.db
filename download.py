c6b1ec7f85358fe0949f60de8c69d65e69e7269a
itay5245 <itay5245@gmail.com>
2020-04-20 19:22:41 +0300

    Added the alf-baked project

--- download.py
+++ download.py
@@ -1,52 +1,58 @@
-def get_users(start_char1, start_char2, file_path:
-	with open(file_path, 'w') as file:
-		for char in range(len(file)):
-			if page[char] == start_char1 and page[char + 1] == start_char2:
-				start_of_table_index = letter
-				break
-	for char in range(len(page) - 1, 0, -1):
-		if page[char] == end_char1 and page[char - 1] == end_char2:
-			end_of_table_index = letter
-			break
-
-	table = 
+import requests
 
 
+def import_table(section, start_char1, start_char2, end_char1, end_char2, path):
+    url = "https://beta.wikiversity.org/w/index.php?title=%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99%D7%9E%D7%97%D7%A9%D7%91%D7%99%D7%9D%D7%91%D7%A9%D7%99%D7%98%D7%AA%D7%91%D7%98%D7%90&action=edit&section=" + section
+
+    r = requests.get(url, 'r')
+
+    page = r.content
+    page = page.decode()
+
+    for letter in range(len(page)):
+        if page[letter] == start_char1 and page[letter + 1] == start_char2:
+            start_of_table_index = letter
+            break
+
+    for letter in range(len(page) - 1, 0, -1):
+        if page[letter] == end_char1 and page[letter - 1] == end_char2:
+            end_of_table_index = letter
+            break
+
+    page = page[start_of_table_index:end_of_table_index]
+
+    with open(path, 'w') as file:
+        file.write(page)
 
 
+def username_slice(page):
+    usernames = {}
+    break_point = 0
+
+    for times in range(page.count('[[User:')):
+        f_index = page.find("[[User:", break_point) + 7
+        l_index = page.find("|", f_index)
+
+        break_point = l_index
+
+        usernames[page[f_index:l_index]] = {}
+
+    return usernames
 
 
+def read_file(path):
+    with open(path, 'r') as file:
+        table_context = file.read()
+    return table_context
 
 
+def main():
+    import_table('14', '{', '|', '}', '|', 'page_context')
+    table_context = read_file("page_context")
+    usernames = username_slice(table_context)
+
+    print(type(usernames.values()))
 
 
-
-
-
-
-def import_and_slice(section, start_char1, start_char2, end_char1, end_char2, path):
-	r = requests.get(url, 'r')
-
-	page = r.content
-	page = page.decode()															# changes type from bytes to string
-
-	for letter in range(len(page)):
-		if page[letter] == start_char1 and page[letter + 1] == start_char2:			# gets the start of the desired table
-			start_of_table_index = letter
-			break
-
-
-	for letter in range(len(page) - 1, 0, -1):
-		if page[letter] == end_char1 and page[letter - 1] == end_char2:				# gets the end of the desired table
-			end_of_table_index = letter
-			break
-
-
-	page = page[start_of_table_index:end_of_table_index]							# inputs the table to page
-
-
-	with open(path, 'w') as file:
-		file.write(page)
-
-url = "https://beta.wikiversity.org/w/index.php?title=%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99_%D7%9E%D7%97%D7%A9%D7%91%D7%99%D7%9D_%D7%91%D7%A9%D7%99%D7%98%D7%AA_%D7%91%D7%98%D7%90&action=edit&section=" + section
-import_and_slice(14, '{', '|', '}', '|', /home/itay5245/page_import)
+if __name__ == 'main':
+    main()
