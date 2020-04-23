e4314d5114cf60d1680bd4a6929c0a07c05273c8
itayboop <62623082+itayboop@users.noreply.github.com>
2020-04-23 21:58:01 +0300

    c_challenges get tabe

---
+++ C_Challenges.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+cells = []
+result = requests.get("https://beta.wikiversity.org/wiki/%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99_%D7%9E%D7%97%D7%A9%D7%91%D7%99%D7%9D_%D7%91%D7%A9%D7%99%D7%98%D7%AA_%D7%91%D7%98%D7%90")
+
+src = result.content
+
+soup = BeautifulSoup(src, 'lxml')
+tables = soup.findChildren('table')
+
+my_table = tables[19]
+
+
+rows = my_table.findChildren(['tr'])
+
+i = 0
+for row in rows[1:]:
+	cells.append([])
+	row = row.find_all('td')
+	cells[i].append(row[0]) 
+	cells[i].append(row[1]) 
+	cells[i].append(row[3])
+	i += 1
+
+print(cells)
